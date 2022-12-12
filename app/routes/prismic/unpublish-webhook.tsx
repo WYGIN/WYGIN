c1e5cf411cee97311fa7a9a7b96802fcd7143228
WYGIN <107541780+WYGIN@users.noreply.github.com>
2022-12-12 13:53:03 +0530

    Create unpublish-webhook.tsx

---
+++ app/routes/prismic/unpublish-webhook.tsx
@@ -0,0 +1,41 @@
+// app/routes/prismic/unpublish-webhook.tsx
+import type { ActionFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { getPrismicClient } from "~/utils/prismicio";
+import { removePrismicDocFromCache } from "~/utils/prismicio.server";
+
+export const action: ActionFunction = async ({ request }) => {
+  if (request.method !== "POST") {
+    return json({ message: "Method not allowed" }, 405);
+  }
+  const payload = await request.json();
+  if (payload.secret !== process.env.PRISMIC_WEBHOOK_SECRET) {
+    return json({ message: "Signature mismatch" }, 401);
+  }
+
+  return deleteCacheEntries(payload);
+};
+
+async function deleteCacheEntries(payload: any) {
+  const client = getPrismicClient();
+  try {
+    if (payload.type !== "api-update" && payload.documents.length <= 0) {
+      throw new Error("No documents to delete");
+    }
+
+    const documents = await client.getAllByIDs(payload.documents);
+
+    // Go over the list of UIDs and use them to delete items in the cache
+    documents
+      .filter((doc) => !!doc.uid)
+      .map(async (doc) => {
+        const uid = doc.uid as string;
+        console.log(`deleting uid ${uid}`);
+        removePrismicDocFromCache(uid);
+      });
+
+    return json({ updated: true }, { status: 200 });
+  } catch (err) {
+    return json({ message: "Error updating cache" }, { status: 500 });
+  }
+}
