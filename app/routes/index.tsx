2b9f90188d12bbeabdf1873101ef409e0bbaac31
WYGIN <107541780+WYGIN@users.noreply.github.com>
2022-12-12 13:45:34 +0530

    Update index.tsx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,37 @@
+// app/routes/index.tsx
+import { SliceZone } from "@prismicio/react";
+import type { PrismicDocument } from "@prismicio/types";
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import Hero from "slices/Hero";
+import { getPrismicClient } from "~/utils/prismicio";
+
+export const loader: LoaderFunction = async () => {
+	const client = getPrismicClient();
+  const customType = "page";
+  const uid = "home";
+
+  try {
+    const doc = await client.getByUID(customType, uid);
+    return json(doc.data);
+  } catch (error) {
+    throw new Response("Not found", {
+      status: 404,
+    });
+  }
+};
+
 export default function Index() {
+  const data = useLoaderData<PrismicDocument["data"]>();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <div>
       <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <SliceZone
+        slices={data.slices}
+        components={{ hero: Hero }}
+      />
     </div>
   );
 }
