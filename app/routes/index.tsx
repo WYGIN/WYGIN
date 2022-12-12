7c6d750d64fa3497031059c7a582dd12e8c3e33d
WYGIN <107541780+WYGIN@users.noreply.github.com>
2022-12-12 16:22:27 +0530

    Update index.tsx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,1 +1,15 @@
+import type { LoaderArgs } from "@remix-run/cloudflare";
+import { json } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
 
+export const loader = async ({ params }: LoaderArgs) => {
+  const indexPage = await 
+};
+
+export default function Index() {
+  return (
+    <>
+      
+    </>
+  )
+}
