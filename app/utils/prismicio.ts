6597c0fdc6cd2afa1ce3cf396dc8cb11199d95eb
WYGIN <107541780+WYGIN@users.noreply.github.com>
2022-12-12 13:48:26 +0530

    Update prismicio.ts

--- app/utils/prismicio.ts
+++ app/utils/prismicio.ts
@@ -4,6 +4,24 @@ import {
   getRepositoryEndpoint,
 } from "@prismicio/client";
 
+// app/utils/prismicio.ts
+import type { LinkResolverFunction } from "@prismicio/helpers";
+
+export const linkResolver: LinkResolverFunction = (
+  document
+) => {
+  if (document.isBroken) {
+    return "/not-found";
+  }
+  if (document.type === "page") {
+		if (document.uid === "home") {
+	    return `/${document.uid}`;
+	  }
+    return `/${document.uid}`;
+  }
+  return "/";
+};
+
 //Add your unique repository name, such as "my-prismic-remix-site" below
 export const repoName = [Insert your unique repository name here];
 // If your Prismic repo is private, add your token here:
