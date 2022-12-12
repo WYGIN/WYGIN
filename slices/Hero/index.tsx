3e6da4bc271a305c22ed7ec242189c12f326a94c
WYGIN <107541780+WYGIN@users.noreply.github.com>
2022-12-12 13:17:53 +0530

    Create index.tsx

---
+++ slices/Hero/index.tsx
@@ -0,0 +1,20 @@
+// slices/Hero/index.tsx
+import { PrismicRichText } from "@prismicio/react";
+import { isFilled } from "@prismicio/helpers";
+import type { SliceComponentProps } from "@prismicio/react";
+import type { HeroSlice } from "types.generated";
+
+type Props = SliceComponentProps<HeroSlice>;
+
+const Hero = ({ slice }: Props) => (
+  <section>
+    {isFilled.title(slice.primary.title) && (
+      <PrismicRichText field={slice.primary.title} />
+    )}
+    {isFilled.richText(slice.primary.description) && (
+      <PrismicRichText field={slice.primary.description} />
+    )}
+  </section>
+);
+
+export default Hero;
