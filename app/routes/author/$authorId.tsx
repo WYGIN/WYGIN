669043b40599a1353b8c2e0fc0a5aeec70e95f21
WYGIN <107541780+WYGIN@users.noreply.github.com>
2022-12-14 16:51:43 +0530

    Update $authorId.tsx

--- app/routes/author/$authorId.tsx
+++ app/routes/author/$authorId.tsx
@@ -1,1 +1,64 @@
-
+import { client } from "../utils/PrismicClient"; 
+  
+ import type { MetaFunction } from "@remix-run/cloudflare"; // or cloudflare/deno 
+ import type { LinksFunction } from "@remix-run/cloudflare"; // or cloudflare/deno 
+  
+ export const links: LinksFunction = () => { 
+   return [] 
+ } 
+  
+ export const meta: MetaFunction = () => { 
+   return { 
+      
+   }; 
+ }; 
+  
+ export const loader = async({ params }) => { 
+   const postData = await client.getByUID('author', params.authorId, { 
+     graphQuery: ` 
+       { 
+          
+       } 
+     `, 
+     predicates: [ 
+        
+     ] 
+   }); 
+   if(!postData || !postData.keys('post').length) { 
+     throw new Response("", { status: 404 }); 
+   } 
+   return json(postData); 
+ } 
+  
+ export default function Author() { 
+   return (); 
+ } 
+  
+ export function CatchBoundary() { 
+   const caught = useCatch<ThrownResponses>(); 
+    
+   switch (caught.status) { 
+     case 401:  
+       return (); 
+     case 404: 
+       return (); 
+   } 
+    
+   return ( 
+     <div> 
+       Something went wrong: {caught.status}{" "} 
+       {caught.statusText} 
+     </div> 
+   ); 
+ } 
+  
+ export function ErrorBoundary({ error }) { 
+   return ( 
+     <div> 
+       <h1>Error</h1> 
+       <p>{error.message}</p> 
+       <p>The stack trace is:</p> 
+       <pre>{error.stack}</pre> 
+     </div> 
+   ); 
+ }
