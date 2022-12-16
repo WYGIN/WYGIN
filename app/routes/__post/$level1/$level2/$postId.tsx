f723ec88e5813586dd765a34472de1d0fce187e5
WYGIN <107541780+WYGIN@users.noreply.github.com>
2022-12-16 17:44:25 +0530

    Update $postId.tsx

--- app/routes/__post/$level1/$level2/$postId.tsx
+++ app/routes/__post/$level1/$level2/$postId.tsx
@@ -1,5 +1,5 @@
 import { client } from "~/utils/PrismicClient";
-import PostBody from ="~/components/PostBody";
+import PostBody from "~/components/PostBody";
 
 import type { MetaFunction } from "@remix-run/cloudflare"; // or cloudflare/deno
 import type { LinksFunction } from "@remix-run/cloudflare"; // or cloudflare/deno
@@ -44,9 +44,15 @@ export function CatchBoundary() {
   
   switch (caught.status) {
     case 401: 
-      return (</>);
+      return (
+        <>
+        </>
+      );
     case 404:
-      return (</>);
+      return (
+        <>
+        </>
+      );
   }
   
   return (
