f2cd09fcc53c13988990287d690ea510341cbdff
WYGIN <107541780+WYGIN@users.noreply.github.com>
2022-12-16 20:15:14 +0530

    Update author.tsx

--- app/routes/author.tsx
+++ app/routes/author.tsx
@@ -1,1 +1,36 @@
+// app/root.tsx
+import { PrismicProvider } from "@prismicio/react";
+import type { MetaFunction } from "@remix-run/cloudflare";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
 
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <PrismicProvider>
+          <Outlet />
+        </PrismicProvider>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
