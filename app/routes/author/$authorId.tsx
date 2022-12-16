eaf8ed0081acd88c09ddd624414b65ec09340dad
WYGIN <107541780+WYGIN@users.noreply.github.com>
2022-12-16 18:27:07 +0530

    Update $authorId.tsx

--- app/routes/author/$authorId.tsx
+++ app/routes/author/$authorId.tsx
@@ -18,8 +18,20 @@ import PostCardLayout from "~/components/PostCardLayout";
    const postData = await client.getByUID('author', params.authorId, { 
      graphQuery: ` 
        { 
-          
-       } 
+          author {
+            name
+            profile
+            bio
+            uid
+            lastPublicationDate
+            links {
+            label
+              ...on link {
+                url
+              }
+            }
+          }
+       } 
      `, 
      predicates: [ 
         
