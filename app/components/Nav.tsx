59a3bd4d11aa4b3c0ee86c84eb8a1d2f2509694b
WYGIN <107541780+WYGIN@users.noreply.github.com>
2022-12-16 15:55:45 +0530

    Update Nav.tsx

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -21,31 +21,35 @@ export default function Nav({data}: {data!: object}) {
           </div>
           <div className='block box-border'>
             <ul className='mb-2.5 box-border block list-disc'>
-              {data.body.nav.link.forEach((link) => {
-                <li className='relative block w-full list-none box-border'>
-                  <a href={link.link} className='text-white no-underline relative block h-[50px] p-4 font-semibold text-center bg-transparent box-border'>{link.label}</a>
-                </li>
-              })
-              <li className='bg-[#4b576d] text-white relative block w-full list-none box-border'>{/*dropdown*/}&nbsp;
-                <a className='text-white no-underline relative block h-[50px] p-4 font-semibold text-center bg-transparent box-border'></a>
-                <div className='hidden block bg-[#535353] my-0 mx-auto box-border text-white list-none '>
-                  <ul className='bg-[#535353] p-0 relative block my-0 mx-auto max-w-[470px] box-border list-style-[circle]'>
-                    <li className='md:p-0 md:mx-[5px] md:mt-0 md:mb-auto relative block border-b-2 box-border list-style-[circle] text-white'>
-                      <a className='text-[#267438] no-underline flex bg-transparent list-style-[circle] '>
-                        <div title='icon' className='xl:inline xl:p-0 xl:pl-[5px] xl:max-w-[70px] grow-0 shrink-0 basis-32 box-border text-[#267438] list-style-[circle] '>
-                          <svg className='inline-block align-middle w-[100px] h-[100px] max-w-[100%] box-border text-[#267438]'></svg>
-                        </div>
-                        <div className='m-0 text-[14.4px] xl:inline xl:max-w-[70%] mt-[5px] pl-[10px] relative text-white no-underline text-left grow shrink basis-auto box-border list-style-[circle]'>
-                          <h3 className='text-xl font-semibold no-underline mt-[20px] mb-[10px] relative box-border block list-style-[circle]'></h3>
-                          <p className='mb-0 mr-[10px] box-border block text-left text-style-[circle]'></p>
-                        </div>
-                      </a>
-                      {/* optional dropdown */}
-                      <div title='dropdown' className='block bg-[#454545] my-0 mx-auto box-border list-style-[circle] '></div>
-                    </li>
-                  </ul>
-                </div>
-              </li>
+              {
+                data.body.nav.link.forEach((link) => {
+                  <li key={link.link} className='relative block w-full list-none box-border'>
+                    <a href={link.link} className='text-white no-underline relative block h-[50px] p-4 font-semibold text-center bg-transparent box-border'>{link.label}</a>
+                  </li>
+                })
+                data.body.nav.linkGroup.forEach((link) => {
+                  <li className='bg-[#4b576d] text-white relative block w-full list-none box-border'>{/*dropdown*/}&nbsp;
+                    <a className='text-white no-underline relative block h-[50px] p-4 font-semibold text-center bg-transparent box-border'></a>
+                    <div className='hidden block bg-[#535353] my-0 mx-auto box-border text-white list-none '>
+                      <ul className='bg-[#535353] p-0 relative block my-0 mx-auto max-w-[470px] box-border list-style-[circle]'>
+                        <li className='md:p-0 md:mx-[5px] md:mt-0 md:mb-auto relative block border-b-2 box-border list-style-[circle] text-white'>
+                          <a className='text-[#267438] no-underline flex bg-transparent list-style-[circle] '>
+                            <div title='icon' className='xl:inline xl:p-0 xl:pl-[5px] xl:max-w-[70px] grow-0 shrink-0 basis-32 box-border text-[#267438] list-style-[circle] '>
+                              <svg className='inline-block align-middle w-[100px] h-[100px] max-w-[100%] box-border text-[#267438]'></svg>
+                            </div>
+                            <div className='m-0 text-[14.4px] xl:inline xl:max-w-[70%] mt-[5px] pl-[10px] relative text-white no-underline text-left grow shrink basis-auto box-border list-style-[circle]'>
+                              <h3 className='text-xl font-semibold no-underline mt-[20px] mb-[10px] relative box-border block list-style-[circle]'></h3>
+                              <p className='mb-0 mr-[10px] box-border block text-left text-style-[circle]'></p>
+                            </div>
+                          </a>
+                          {/* optional dropdown */}
+                          <div title='dropdown' className='block bg-[#454545] my-0 mx-auto box-border list-style-[circle] '></div>
+                        </li>
+                      </ul>
+                    </div>
+                  </li>
+                })
+              }
             </ul>
           </div>
           <div title='row' className='box-border block mx-0 md:px-[5%] text-xl'>
